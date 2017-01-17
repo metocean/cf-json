052a06e824a6d025eeb86d42512cb0708e67c689
sebastien <sebastien@delauxs-desktop.rag.metocean.co.nz>
2017-01-17 15:38:48 +1300

    Adding some error handling

--- nc_json.py
+++ nc_json.py
@@ -24,44 +24,63 @@ def print_val(val):
     
     
 class NCDataset(Dataset):
+    """
+    Class derived from the Dataset class of NetCDF4 library and implementing methods
+    that dump to and load from JSON.
+    """
     def to_dict(self):
         """
         Dumps the dataset as an ordered dictionnary following the same conventions as ncdump.
         Assumes the Dataset is CF complient.
         """
         res=OrderedDict()
-        res['dimensions']=OrderedDict()
-        for dim in self.dimensions:
-            res['dimensions'][dim]=len(self.dimensions[dim])
+        try:
+            res['dimensions']=OrderedDict()
+            for dim in self.dimensions:
+                res['dimensions'][dim]=len(self.dimensions[dim])
+        except:
+            print('Failed to export dimensions')
+            raise
 
         res['variables']=OrderedDict()
         for var in self.variables:
-            res['variables'][var]={'type':self.variables[var].datatype.name,
-                                   'dimensions':[d for d in self.variables[var].dimensions],
-                                   'attributes':OrderedDict()}
-            for att in self.variables[var].ncattrs():
-                res['variables'][var]['attributes'].update({str(att):print_val(self.variables[var].getncattr(att))})
+            try:
+                res['variables'][var]={'type':self.variables[var].datatype.name,
+                                       'dimensions':[d for d in self.variables[var].dimensions],
+                                       'attributes':OrderedDict()}
+                for att in self.variables[var].ncattrs():
+                    res['variables'][var]['attributes'].update({str(att):print_val(self.variables[var].getncattr(att))})
+            except:
+                print('Failed to export variable %s description or attributes'%(var))
+                raise
 
-        res['global_attributes']=OrderedDict()
-        for att in self.ncattrs():
-            res['global_attributes'].update({str(att):print_val(self.getncattr(att))})
+        try:
+            res['global_attributes']=OrderedDict()
+            for att in self.ncattrs():
+                res['global_attributes'].update({str(att):print_val(self.getncattr(att))})
+        except:
+            print('Failed to export all global_attribute %s'%(att))
 
         res['data']=OrderedDict()
         for var in self.variables:
+            try:
 
-            vals=numpy.array(self.variables[var]).flatten().tolist()
+                vals=numpy.array(self.variables[var]).flatten().tolist()
             
-            #if var == 'time' and 'units' in self.variables[var].ncattrs():
-            #    if 'calendar' in self.variables[var].ncattrs():
-            #        vals=[str(t) for t in num2date(vals, self.variables[var].getncattr('units'),
-            #        self.variables[var].getncattr('calendar'))]
-            #        res['data'].update({var:vals})
-            #    else:
-            #        vals=[str(t) for t in num2date(vals, self.variables[var].getncattr('units'))]
-            #        res['data'].update({var:vals})
-            #else:
-            vals=[print_val(val) for val in vals]
-            res['data'].update({var:vals})
+                  #if var == 'time' and 'units' in self.variables[var].ncattrs():
+                  #    if 'calendar' in self.variables[var].ncattrs():
+                  #        vals=[str(t) for t in num2date(vals, self.variables[var].getncattr('units'),
+                  #        self.variables[var].getncattr('calendar'))]
+                  #        res['data'].update({var:vals})
+                  #    else:
+                  #        vals=[str(t) for t in num2date(vals, self.variables[var].getncattr('units'))]
+                  #        res['data'].update({var:vals})
+                  #else:
+                vals=[print_val(val) for val in vals]
+                res['data'].update({var:vals})
+            except:
+                  print('Failed to export values for variable %s'%(var))
+                  raise
 
         return res
 
@@ -73,37 +92,54 @@ class NCDataset(Dataset):
         dico=self.to_dict()
         return json.dumps(dico, indent=indent, separators=separators)
 
+    
     def from_json(self, js):
         """
         Populates a NCDataset object from its JSON representation.
         """
-        dico=json.JSONDecoder(object_pairs_hook=OrderedDict).decode(js)
-        
-        for dim in dico['dimensions']:
-            self.createDimension(dim,dico['dimensions'][dim])
+        try:
+            dico=json.JSONDecoder(object_pairs_hook=OrderedDict).decode(js)
+        except:
+            print('Could not decode JSON')
+            raise
 
-        self.setncatts(dico['global_attributes'])
-
-        for var in dico['variables']:
-            if '_FillValue' in dico['variables'][var]['attributes']:
-                self.createVariable(var, dico['variables'][var]['type'],
-                                    dimensions=dico['variables'][var]['dimensions'],
-                                    fill_value=dico['variables'][var]['attributes']['_FillValue'])
-            else:
-                self.createVariable(var, dico['variables'][var]['type'],
-                                    dimensions=dico['variables'][var]['dimensions'])
-                
-            for att in dico['variables'][var]['attributes']:
-                if att != '_FillValue':
-                    self.variables[var].setncattr(att, dico['variables'][var]['attributes'][att])
+        try:
+            for dim in dico['dimensions']:
+                self.createDimension(dim,dico['dimensions'][dim])
+        except:
+            print('Issue occured when creating dimension %s'%dim)
+            raise
             
-            self.variables[var][:]=numpy.array(dico['data'][var]).reshape(self.variables[var].shape)
-        
-        
-#dst=NCDataset("/tmp/query_results/query.b2d4a6f8-d6c4-11e6-9a1c-0242ac130006.nc")
-#js = dst.json_dumps(indent=4, separators=(',', ': '))
+        try:
+            self.setncatts(dico['global_attributes'])
+        except:
+            print('Issue occured when inserting global attributes')
 
-#new_dst=NCDataset('/tmp/seb.nc','w')
+        
+        for var in dico['variables']:
+            try:
+                if '_FillValue' in dico['variables'][var]['attributes']:
+                    self.createVariable(var, dico['variables'][var]['type'],
+                                        dimensions=dico['variables'][var]['dimensions'],
+                                        fill_value=dico['variables'][var]['attributes']['_FillValue'])
+                else:
+                    self.createVariable(var, dico['variables'][var]['type'],
+                                        dimensions=dico['variables'][var]['dimensions'])
+            except:
+                print('Could not create variable %s'%var)
+                raise
 
-#new_dst.from_json(js)
-#new_dst.close()
+            for att in dico['variables'][var]['attributes']:
+                try:
+                    if att != '_FillValue':
+                        self.variables[var].setncattr(att, dico['variables'][var]['attributes'][att])
+                except:
+                    print('Failed to set variable %s attribute %s to %s'%(var,
+                                                                          att,
+                                                                          str(dico['variables'][var]['attributes'][att])))
+                        
+            try:
+                self.variables[var][:]=numpy.array(dico['data'][var]).reshape(self.variables[var].shape)
+            except:
+                print('Could not populate variable %s'%var)
+                
